24738439003220152de72f7802ee1d1af2482786
Ronald <pietjebell31@hotmail.com>
2008-05-26 17:33:49 +0000

    Added a file remotely
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/mytube/trunk@2449 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ modules/mytube/language/nederlands/blocks.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Module: MyTube
+ */
+ 
+// Blocks
+define("_MB_XTUBE_DISP","Tonen");
+define("_MB_XTUBE_FILES","Videos");
+define("_MB_XTUBE_CHARS","Lengte van de titel");
+define("_MB_XTUBE_LENGTH"," karakters");
+
+// Version 1.04
+define( "_MB_XTUBE_ID", "ID" );
+define( "_MB_XTUBE_CLIENT", "Client Naam" );
+define( "_MB_XTUBE_BANNERID", "Banner ID" );
+define( "_MB_XTUBE_IMPMADE", "Impressies" );
+define( "_MB_XTUBE_IMPLEFT", "Impr. over" );
+define( "_MB_XTUBE_CLICKS", "Kliks" );
+define( "_MB_XTUBE_CATTITLE", "Categorie" );
+define( "_MB_XTUBE_UNLIMIT", "Ongelimiteerd" );
+define( '_MB_XTUBE_DATEFORMAT', 'Datum weergave' );
+define( '_MB_XTUBE_DATEFORMATMANUAL', 'Zie <a href="http://docs.php.net/manual/nl/function.date.php" target="_blank">PHP handleiding</a>' );
+?>
