b580c3ab237f8a47136422a96afc7062b5885ede
Ronald <pietjebell31@hotmail.com>
2008-05-26 17:18:03 +0000

    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/mytube/trunk/modules/mytube@2441 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ language/spanish/blocks.php
@@ -0,0 +1,18 @@
+<?php
+//Traducción de Evasan y Debianus para http://es.impresscms.org
+/**
+* $Id: blocks.php
+ * Module: MyTube
+ * Language: english
+ */
+ 
+// Blocks
+define("_MB_XTUBE_DISP","Pantalla");
+define("_MB_XTUBE_FILES","Vídeos");
+define("_MB_XTUBE_CHARS","Longitud del título");
+define("_MB_XTUBE_LENGTH","Caracteres");
+
+// Version 1.04
+define( "_MB_XTUBE_DATEFORMAT", "Date format" );
+define( "_MB_XTUBE_DATEFORMATMANUAL", 'See <a href="http://jp.php.net/manual/en/function.date.php" target="_blank">PHP manual</a>' );
+?>
