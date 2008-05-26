45fcb2ce90e3155839ca0c3423bb965b99213f26
Ronald <pietjebell31@hotmail.com>
2008-05-26 19:42:40 +0000

    Added banner sponsoring, needs translation
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/mytube/trunk@2458 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ modules/mytube/language/spanish/blocks.php
@@ -0,0 +1,26 @@
+<?php
+//Traducción de Evasan y Debianus para http://es.impresscms.org
+/**
+* $Id: blocks.php
+ * Module: MyTube
+ * Language: english
+ */
+ 
+// Blocks
+define("_MB_XTUBE_DISP","Pantalla");
+define("_MB_XTUBE_FILES","Vídeos");
+define("_MB_XTUBE_CHARS","Longitud del título");
+define("_MB_XTUBE_LENGTH","Caracteres");
+
+// Version 1.04
+define( "_MB_XTUBE_ID", "ID" );
+define( "_MB_XTUBE_CLIENT", "Client Name" );
+define( "_MB_XTUBE_BANNERID", "Banner ID" );
+define( "_MB_XTUBE_IMPMADE", "Impr. made" );
+define( "_MB_XTUBE_IMPLEFT", "Impr. left" );
+define( "_MB_XTUBE_CLICKS", "Clicks" );
+define( "_MB_XTUBE_CATTITLE", "Category" );
+define( "_MB_XTUBE_UNLIMIT", "Unlimited" );
+define( "_MB_XTUBE_DATEFORMAT", "Date format" );
+define( "_MB_XTUBE_DATEFORMATMANUAL", 'See <a href="http://jp.php.net/manual/en/function.date.php" target="_blank">PHP manual</a>' );
+?>
