4ce4c5e039612829f76cf1a08a0a08967bbf3658
Ronald <pietjebell31@hotmail.com>
2008-05-27 21:34:29 +0000

    Changed mytube into XOOPSTUBE_DIRNAME
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/mytube/trunk/modules/mytube@2505 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ include/config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * $Id: config.php
+ * Module: MyTube
+ */
+ 
+// WARNING: ONCE SET DO NOT CHANGE! Improper use will render this module useless and unworkable.
+// Only Change if you know what you are doing.
+
+if ( !defined( "xoopstube_broken" ) ) define( "xoopstube_broken", "xoopstube_broken" );
+if ( !defined( "xoopstube_cat" ) ) define( "xoopstube_cat", "xoopstube_cat" );
+if ( !defined( "xoopstube_videos" ) ) define( "xoopstube_videos", "xoopstube_videos" );
+if ( !defined( "xoopstube_mod" ) ) define( "xoopstube_mod", "xoopstube_mod" );
+if ( !defined( "xoopstube_votedata" ) ) define( "xoopstube_votedata", "xoopstube_votedata" );
+if ( !defined( "xoopstube_indexpage" ) ) define( "xoopstube_indexpage", "xoopstube_indexpage" );
+if ( !defined( "xoopstube_altcat" ) ) define( "xoopstube_altcat", "xoopstube_altcat" );
+if ( !defined( "xoopstube_url" ) ) define( "xoopstube_url", XOOPS_URL . "/modules/" . XOOPSTUBE_DIRNAME . "/" );
+
+?>
