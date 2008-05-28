02c0131cb4570b306c17239757def4d3a5112f5e
Sina Asghari <stranger@impresscms.org>
2008-05-28 14:42:04 +0000

    A little fix for my last commit!
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/mytube/trunk@2509 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- modules/mytube/header.php
+++ modules/mytube/header.php
@@ -6,15 +6,14 @@
 
 include_once '../../mainfile.php';
 
-if( !defined("XOOPSTUBE_DIRNAME") ){
-	define("XOOPSTUBE_DIRNAME", 'mytube');
-}
+$mydirname = basename( dirname( __FILE__ ) ) ;
+$mydirpath = dirname( __FILE__ ) ;
 
-include XOOPS_ROOT_PATH . '/modules/' . XOOPSTUBE_DIRNAME . '/include/config.php';
-include XOOPS_ROOT_PATH . '/modules/' . XOOPSTUBE_DIRNAME . '/include/functions.php';
-include XOOPS_ROOT_PATH . '/modules/' . XOOPSTUBE_DIRNAME . '/include/video.php';
-include XOOPS_ROOT_PATH . '/modules/' . XOOPSTUBE_DIRNAME . '/sbookmarks.php';
-include_once XOOPS_ROOT_PATH . '/modules/' . XOOPSTUBE_DIRNAME . '/class/class_thumbnail.php';
+include XOOPS_ROOT_PATH . '/modules/' . $mydirname . '/include/config.php';
+include XOOPS_ROOT_PATH . '/modules/' . $mydirname . '/include/functions.php';
+include XOOPS_ROOT_PATH . '/modules/' . $mydirname . '/include/video.php';
+include XOOPS_ROOT_PATH . '/modules/' . $mydirname . '/sbookmarks.php';
+include_once XOOPS_ROOT_PATH . '/modules/' . $mydirname . '/class/class_thumbnail.php';
 include_once XOOPS_ROOT_PATH . '/class/pagenav.php';
 include_once XOOPS_ROOT_PATH . '/class/xoopstree.php';
 
@@ -22,7 +21,7 @@ if ( !file_exists( "language/" . $xoopsConfig['language'] . "/main.php" ) ) {
     include "language/" . $xoopsConfig['language'] . "/main.php";
 } 
 
-include_once XOOPS_ROOT_PATH . '/modules/' . XOOPSTUBE_DIRNAME . '/class/myts_extended.php';
+include_once XOOPS_ROOT_PATH . '/modules/' . $mydirname . '/class/myts_extended.php';
 $xtubemyts = new xtubeTextSanitizer(); // MyTextSanitizer object
 
 global $xoopModuleConfig;
