aa48e330e31215e357da0ef7459f360b0afdb6dc
Ronald <pietjebell31@hotmail.com>
2008-05-29 06:22:48 +0000

    Updated German language files [wuddel]
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/mytube/trunk@2517 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ modules/mytube/language/german/blocks.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * $Id: blocks.php
+ * Module: MyTube
+* Language: german by wuddel (myxoops.org/impresscms.de)
+ */
+ 
+// Blocks
+define("_MB_XTUBE_DISP","Anzeige");
+define("_MB_XTUBE_FILES","Videos");
+define("_MB_XTUBE_CHARS","L&Auml;nge des Titels");
+define("_MB_XTUBE_LENGTH"," Zeichen");
+
+// Version 1.04
+define( "_MB_XTUBE_ID", "ID" );
+define( "_MB_XTUBE_CLIENT", "Klient Name" );
+define( "_MB_XTUBE_BANNERID", "Banner ID" );
+define( "_MB_XTUBE_IMPMADE", "erreichte Impr." );
+define( "_MB_XTUBE_IMPLEFT", "offene Impr." );
+define( "_MB_XTUBE_CLICKS", "Aufrufe" );
+define( "_MB_XTUBE_CATTITLE", "Kategorie" );
+define( "_MB_XTUBE_UNLIMIT", "Unbegrenzt" );
+define( "_MB_XTUBE_DATEFORMAT", "Datumformat" );
+define( "_MB_XTUBE_DATEFORMATMANUAL", 'Siehe <a href="http://jp.php.net/manual/de/function.date.php" target="_blank">PHP Syntax</a>' );
+?>
