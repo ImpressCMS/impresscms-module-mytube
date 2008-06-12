7067451f875a535a7931d59a6e037bfa4cfb8141
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-06-12 22:57:24 +0000

    modules - mytube - part 12 (blocks.php) translation finished with utf-8...
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/mytube/trunk@2816 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- modules/mytube/language/portuguesebr/blocks.php
+++ modules/mytube/language/portuguesebr/blocks.php
@@ -15,13 +15,13 @@ define("_MB_XTUBE_LENGTH"," caracteres");
 
 // Version 1.04
 define( "_MB_XTUBE_ID", "ID" );
-define( "_MB_XTUBE_CLIENT", "Client Name" );
+define( "_MB_XTUBE_CLIENT", "Nome do Cliente" );
 define( "_MB_XTUBE_BANNERID", "Banner ID" );
-define( "_MB_XTUBE_IMPMADE", "Impr. made" );
+define( "_MB_XTUBE_IMPMADE", "Impr. feitas" );
 define( "_MB_XTUBE_IMPLEFT", "Impr. left" );
-define( "_MB_XTUBE_CLICKS", "Clicks" );
-define( "_MB_XTUBE_CATTITLE", "Category" );
-define( "_MB_XTUBE_UNLIMIT", "Unlimited" );
-define( "_MB_XTUBE_DATEFORMAT", "Date format" );
+define( "_MB_XTUBE_CLICKS", "Cliques" );
+define( "_MB_XTUBE_CATTITLE", "Categoria" );
+define( "_MB_XTUBE_UNLIMIT", "Ilimitado" );
+define( "_MB_XTUBE_DATEFORMAT", "Formato de Data" );
 define( "_MB_XTUBE_DATEFORMATMANUAL", 'Veja também o <a href="http://jp.php.net/manual/pt_BR/function.date.php" target="_blank">Manual do PHP</a>' );
 ?>
