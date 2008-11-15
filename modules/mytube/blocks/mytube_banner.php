0b8e3e27b51a565b3bb6bb0e5eb460bdacf900c1
Ronald <pietjebell31@hotmail.com>
2008-11-15 12:04:39 +0000

    Fixed: banner statistics block
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/mytube/trunk@6921 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- modules/mytube/blocks/mytube_banner.php
+++ modules/mytube/blocks/mytube_banner.php
@@ -5,25 +5,21 @@
  * Developer: McDonald
  * Licence: GNU
  */
-if (!defined('XOOPS_ROOT_PATH')) {
-	die('XOOPS root path not defined');
-}
-$mydirname = basename( dirname(  dirname( __FILE__ ) ) ) ;
-$mydirpath = dirname( dirname( __FILE__ ) ) ;
 
 function b_mytube_banner_show( $options ) {
+	
     global $xoopsDB;
-
+	
+	$mydirname = basename( dirname(  dirname( __FILE__ ) ) ) ;
+	
     $block = array();
     $time = time();
     $modhandler = &xoops_gethandler( 'module' );
-    $wflModule = &$modhandler -> getByDirname($mydirname);
+    $mytubeModule = &$modhandler -> getByDirname($mydirname);
     $config_handler = &xoops_gethandler( 'config' );
-    $wflModuleConfig = &$config_handler -> getConfigsByCat( 0, $wflModule -> getVar( 'mid' ) );
+    $mytubeModuleConfig = &$config_handler -> getConfigsByCat( 0, $mytubeModule -> getVar( 'mid' ) );
 
-    $wfmyts = &MyTextSanitizer :: getInstance();
-
-    $result = $xoopsDB -> query( "SELECT a.cid as acid, a.title, a.client_id, a.banner_id, b.bid, b.cid, b.imptotal, b.impmade, b.clicks FROM " . $xoopsDB -> prefix( 'xoopstube_cat' ) . " a, " . $xoopsDB -> prefix( 'banner' ) . " b WHERE (b.cid = a.client_id) OR (b.bid = a.banner_id) ORDER BY b.cid, b.bid, a.title ASC" );
+    $result = $xoopsDB -> query( 'SELECT a.cid as acid, a.title, a.client_id, a.banner_id, b.bid, b.cid, b.imptotal, b.impmade, b.clicks FROM ' . $xoopsDB -> prefix( 'xoopstube_cat' ) . ' a, ' . $xoopsDB -> prefix( 'banner' ) . ' b WHERE (b.cid = a.client_id) OR (b.bid = a.banner_id) ORDER BY b.cid, b.bid, a.title ASC' );
 
     while ( $myrow = $xoopsDB -> fetchArray( $result ) ) {
 
@@ -31,29 +27,29 @@ function b_mytube_banner_show( $options ) {
         $clicks = $myrow['clicks'];
         $imptotal = $myrow['imptotal'];
         $bannerload = array();
-        $result2 = $xoopsDB -> query( "SELECT name FROM " . $xoopsDB -> prefix( 'bannerclient' ) . " WHERE cid=" . intval($myrow['cid']) );
+        $result2 = $xoopsDB -> query( 'SELECT name FROM ' . $xoopsDB -> prefix( 'bannerclient' ) . ' WHERE cid=' . intval( $myrow['cid'] ) );
         $myclient = $xoopsDB -> fetchArray( $result2 );
         if ( $impmade == 0 ) {
             $percent = 0;
         } else {
-            $percent = substr(100 * $clicks / $impmade, 0, 5);
+            $percent = substr( 100 * $clicks / $impmade, 0, 5 );
         }
         if ( $imptotal == 0 ) {
             $left = 'Unlimited';
         } else {
             $left = intval($imptotal-$impmade);
         }
-        $bannerload['cat'] = intval($myrow['acid']);
-        $bannerload['bid'] = intval($myrow['bid']);
-        $bannerload['cid'] = intval($myrow['cid']);
-        $bannerload['imptotal'] = intval($myrow['imptotal']);
-        $bannerload['impmade'] = intval($myrow['impmade']);
+        $bannerload['cat'] = intval( $myrow['acid'] );
+        $bannerload['bid'] = intval( $myrow['bid'] );
+        $bannerload['cid'] = intval( $myrow['cid'] );
+        $bannerload['imptotal'] = intval( $myrow['imptotal'] );
+        $bannerload['impmade'] = intval( $myrow['impmade'] );
         $bannerload['impleft'] = $left;
-        $bannerload['clicks'] = intval($myrow['clicks']);
+        $bannerload['clicks'] = intval( $myrow['clicks'] );
         $bannerload['client'] = $myclient['name'];
         $bannerload['percent'] = $percent;
         $bannerload['cattitle'] = $myrow['title'];
-        $bannerload['dirname'] = $wflModule -> getVar( 'dirname' );
+        $bannerload['dirname'] = $mytubeModule -> getVar( 'dirname' );
         $block['banners'][] = $bannerload;
     }
     unset( $_block_check_array );
@@ -61,7 +57,7 @@ function b_mytube_banner_show( $options ) {
 }
 
 function b_mytube_banner_edit( $options ) {
-    $form = "";
+    $form = '';
     return $form;
 } 
 
