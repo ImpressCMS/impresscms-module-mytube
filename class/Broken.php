72423199525ad71fab56d8bf1942cc46f520729a
Ronald <pietjebell31@hotmail.com>
2011-10-13 22:20:32 +0000

    Added classes and modified installation/update procedures.
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/mytube/trunk/modules/mytube@22887 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ class/Broken.php
@@ -0,0 +1,36 @@
+<?php
+/**
+* MyTube - a multicategory video management module for ImpressCMS
+*
+* Based upon WF-Links 1.06
+*
+* File: class/Broken.php
+*
+* @copyright	http://www.xoops.org/ The XOOPS Project
+* @copyright	XOOPS_copyrights.txt
+* @copyright	http://www.impresscms.org/ The ImpressCMS Project
+* @license		GNU General Public License (GPL)
+*				a copy of the GNU license is enclosed.
+* ----------------------------------------------------------------------------------------------------------
+* 				MyTube
+* @since		1.00
+* @author		McDonald
+* @version		$Id$
+*/
+
+defined( 'ICMS_ROOT_PATH' ) or die ( 'ICMS root path not defined' );
+
+class mod_mytube_Broken extends icms_ipf_seo_Object {
+	public function __construct( &$handler ) {
+		icms_ipf_object::__construct( $handler );
+
+		$this -> quickInitVar( 'reportid', XOBJ_DTYPE_INT, true );
+		$this -> quickInitVar( 'lid', XOBJ_DTYPE_INT, true );
+		$this -> quickInitVar( 'sender', XOBJ_DTYPE_INT, false );
+		$this -> quickInitVar( 'ip', XOBJ_DTYPE_TXTBOX, false );
+		$this -> quickInitVar( 'date', XOBJ_DTYPE_TXTBOX, false );
+		$this -> quickInitVar( 'confirmed', XOBJ_DTYPE_INT, false );
+		$this -> quickInitVar( 'acknowledged', XOBJ_DTYPE_INT, false );
+		$this -> quickInitVar( 'title', XOBJ_DTYPE_TXTBOX, false );
+	}
+}
---
+++ include/onupdate.inc.php
@@ -0,0 +1,36 @@
+<?php
+/**
+* MyTube - a multicategory video management module for ImpressCMS
+*
+* Based upon WF-Links 1.06
+*
+* File: include/onupdate.inc.php
+*
+* @copyright	http://www.xoops.org/ The XOOPS Project
+* @copyright	XOOPS_copyrights.txt
+* @copyright	http://www.impresscms.org/ The ImpressCMS Project
+* @license		GNU General Public License (GPL)
+*				a copy of the GNU license is enclosed.
+* ----------------------------------------------------------------------------------------------------------
+* 				MyTube
+* @since		1.00
+* @author		McDonald
+* @version		$Id$
+*/
+ 
+if ( !defined( 'ICMS_ROOT_PATH' ) ) { die( 'ICMS root path not defined' ); }
+
+define( 'MYTUBE_DB_VERSION', 1 );
+
+function icms_module_update_mytube( $module ) {
+	return true;
+}
+
+function icms_module_install_mytube( $module ) {	
+	$sql = "INSERT INTO " . icms::$xoopsDB -> prefix( 'mytube_indexpage' ) . " (indeximage,indexheading,indexheader,indexfooter,indexheaderalign,indexfooteralign) VALUES ('logo-en.png','MyTube','Please report any unavailable video clips by using the [i]Report Broken[/i] option. Thanks!','<a href=\"http://www.adobe.com/go/getflashplayer\" target=\"_blank\"><img src=\"images/get_flash_player.png\" alt=\"Get Adobe Flash Player\" border=\"0\" /></a>','left','left')";
+	if ( !$result = icms::$xoopsDB -> queryF( $sql ) ) {
+		icms::$logger -> handleError( E_USER_WARNING, $sql, __FILE__, __LINE__ );
+		return false;
+	}
+	return TRUE;
+}
